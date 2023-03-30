6418b10398d4aea2117e829d948a74aee9f55b40
Steven Araya Gonzalez <Stevenaraya005@gmail.com>
2023-03-29 22:33:37 -0600

    changes

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const login = require('./routes/login');
 //Inicializar express
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
